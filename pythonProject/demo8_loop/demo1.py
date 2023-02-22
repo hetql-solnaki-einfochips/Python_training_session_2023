2380606229bc63013b4567652165224241e8e7df
Hetal Solanki <Hetal.Solanki@einfochips.com>
2023-02-22 11:50:40 +0530

    Added for loop

---
+++ pythonProject/demo8_loop/demo1.py
@@ -0,0 +1,13 @@
+colors=['blue', 'green', 'yellow']
+
+for i in colors:
+    print(colors[0])
+
+
+numbers=[45,98,75,65,24,88,74,56,75]
+
+for i in range(0,len(numbers)):
+    if numbers[i]<=50:
+        print(numbers[i])
+    else:
+        print("Number are greater than 50")
